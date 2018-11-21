9c9ad8d13da23fc3976ce7d792cc30363626e23c
udaykusupati <you@example.com>
2018-11-21 01:47:38 -0600

    removed data

--- data/annotated/vocabGen.py
+++
@@ -1,3 +0,0 @@
-import sentencepiece as spm
-spm.SentencePieceTrainer.Train('--input=conala-trainnodev.intent --model_prefix=conala-trainnodev-intent-4000 --vocab_size=4000 --max_sentence_length=10000 --hard_vocab_limit=False')
-spm.SentencePieceTrainer.Train('--input=conala-trainnodev.snippet --model_prefix=conala-trainnodev-snippet-4000 --vocab_size=4000 --max_sentence_length=10000 --hard_vocab_limit=False')
