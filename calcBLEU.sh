e4dbe6a10944ba29ed82ae406db942a4f257fb35
Ravi Teja A V <avteja@gmail.com>
2018-12-04 23:00:47 -0600

    Modified eval logging code

--- calcBLEU.sh
+++ calcBLEU.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python preproc/seq2seq_output_to_code.py results/mypreds.hyp data/conala-corpus/conala-$1.json.seq2seq results/$1.json
+python preproc/seq2seq_output_to_code.py $2/mypreds$3.hyp data/conala-corpus/conala-$1.json.seq2seq results/$1.json
 python eval/conala_eval.py --strip_ref_metadata --input_ref data/conala-corpus/conala-$1.json --input_hyp results/$1.json --output_dir $2 --epoch $3
