b96ba624bc1b1ae28a7101ac3cac04c664f96a55
Uday Kusupati <kusupatiuday@gmail.com>
2018-12-04 22:50:45 -0600

    clean cycle_train and new models

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,1 @@
-python3 tct_train.py -data conala-trainnodev+mined-100k+test-16000.pt -snippet_model data/annotated/conala-trainnodev+mined-100k-snippet-16000.model -n_layers 2 -epoch 100 -batch_size 32 -beam_size 2 -dropout 0.2 -alpha 2.0 -save_model_dir /scratch/cluster/avrteja/nlp2/tct_train_mined_check -test_epoch 1
+python3 ctc_train.py -data conala-train+dev+test.pt -snippet_model data/annotated/conala-trainnodev-snippet-4000.model -n_layers 2 -epoch 100 -batch_size 16 -beam_size 2 -dropout 0.2 -alpha 2.0 -save_model_dir /scratch/cluster/uday/assam4 -test_epoch 5
